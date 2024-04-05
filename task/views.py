63b9da366f7d58da45c9ff6862dce3a40918cfec
Carlos D Martinez <Al234498@alumnos.uacj.mx>
2024-04-05 15:18:59 -0600

    Primer Commit

---
+++ task/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib.auth import login, logout
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request): 
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username = request.POST['username'], password = request.POST['password1']) 
+                user.save()
+                login(request, user)
+                return redirect('main')
+            except:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'Username already exists.'
+                })
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'Password do not match'
+        })
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def main(request):
+    return render(request, 'main.html')
